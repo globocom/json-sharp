303123a86ad0f7bd76ef035d29d735d77f928a5b
Enrico <rico.bl@gmail.com>
2015-10-02 14:53:24 -0300

    Added basic switch / case operation

--- src/json-sharp.js
+++ src/json-sharp.js
@@ -2,8 +2,37 @@
 
 var JSONSharp = {
     process: function (obj, context) {
+        var key;
         var result = this._clone(obj);
+        for (key in result) {
+            if (!result.hasOwnProperty(key)) {
+                continue;
+            }
+
+            if ((key.charAt(0) === '#') && this.operations[key]) {
+                return this.operations[key](result[key], context);
+            }
+        }
         return result;
+    },
+    operations: {
+        '#switch': function (obj, context) {
+            var result;
+            var value = context[obj['#property']];
+            var options = obj['#case'];
+
+            result = options[value];
+            if (result !== undefined) {
+                return result;
+            }
+
+            result = options['#default'];
+            if (result !== undefined) {
+                return result;
+            }
+
+            return undefined;
+        }
     },
     _clone: function (obj) {
         return JSON.parse(JSON.stringify(obj));
--- tests/spec.js
+++ tests/spec.js
@@ -20,9 +20,44 @@ describe('json-sharp', function () {
         });
 
         it('should return the value when it is not an array or object', function () {
-            var obj = "str";
+            var obj = 'str';
             var result = JSONSharp.process(obj, {});
             expect(result).to.be.equal(obj);
+        });
+    });
+
+    describe('#switch', function () {
+        var baseObj = {
+            '#switch': {
+                '#property': 'option',
+                '#case': {
+                    a: 'AAA',
+                    b: 'BBB',
+                }
+            }
+        };
+
+        beforeEach(function () {
+            this.obj = JSONSharp._clone(baseObj);
+        });
+
+        it('should return matching value', function () {
+            var context = {option: 'a'};
+            var result = JSONSharp.process(this.obj, context);
+            expect(result).to.be.deep.equal('AAA');
+        });
+
+        it('should fallback to default value', function () {
+            this.obj['#switch']['#case']['#default'] = 'CCC';
+            var context = {option: '???'};
+            var result = JSONSharp.process(this.obj, context);
+            expect(result).to.be.deep.equal('CCC');
+        });
+
+        it('should return undefined when there is no fallback', function () {
+            var context = {option: '???'};
+            var result = JSONSharp.process(this.obj, context);
+            expect(result).to.be.undefined;
         });
     });
 });
