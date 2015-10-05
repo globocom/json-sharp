69d97e8a229029f622b0e9cfaaaf3801bf243536
Enrico <rico.bl@gmail.com>
2015-10-05 10:34:16 -0300

    Added support for UMD

--- src/json-sharp.js
+++ src/json-sharp.js
@@ -1,57 +1,71 @@
+/* global define */
+
 'use strict';
 
-// TODO: find a cross-platform (node, browser) way to import underscore
-var _ = _ || require('underscore'); // jshint ignore:line
-
-var JSONSharp = {
-    process: function (obj, context) {
-        var isArrayOrObject = (typeof obj === 'object');
-        if (!isArrayOrObject) {
-            return obj;
-        }
-
-        var key, operation, options;
-        var result = this._clone(obj);
-
-        for (key in result) {
-            if (!result.hasOwnProperty(key)) {
-                continue;
-            }
-
-            operation = this.operations[key];
-            if (operation !== undefined) {
-                options = result[key];
-                return this.process(this.operations[key](options, context), context);
-            }
-        }
-        return result;
-    },
-    operations: {
-        '#merge': function (obj) {
-            // TODO: make it deep.. (lodash? jQuery?)
-            return _.extend.apply(this, obj);
-        },
-        '#switch': function (obj, context) {
-            var result;
-            var value = context[obj['#property']];
-            var options = obj['#case'];
-
-            result = options[value];
-            if (result !== undefined) {
-                return result;
-            }
-
-            result = options['#default'];
-            if (result !== undefined) {
-                return result;
-            }
-
-            return undefined;
-        }
-    },
-    _clone: function (obj) {
-        return JSON.parse(JSON.stringify(obj));
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['underscore'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('underscore'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root._);
     }
-};
+}(this, function (_) { /* jshint ignore:line */
+    var JSONSharp = {
+        process: function (obj, context) {
+            var isArrayOrObject = (typeof obj === 'object');
+            if (!isArrayOrObject) {
+                return obj;
+            }
 
-module.exports = {JSONSharp: JSONSharp};
+            var key, operation, options;
+            var result = this._clone(obj);
+
+            for (key in result) {
+                if (!result.hasOwnProperty(key)) {
+                    continue;
+                }
+
+                operation = this.operations[key];
+                if (operation !== undefined) {
+                    options = result[key];
+                    return this.process(this.operations[key](options, context), context);
+                }
+            }
+            return result;
+        },
+        operations: {
+            '#merge': function (obj) {
+                // TODO: make it deep.. (lodash? jQuery?)
+                return _.extend.apply(this, obj);
+            },
+            '#switch': function (obj, context) {
+                var result;
+                var value = context[obj['#property']];
+                var options = obj['#case'];
+
+                result = options[value];
+                if (result !== undefined) {
+                    return result;
+                }
+
+                result = options['#default'];
+                if (result !== undefined) {
+                    return result;
+                }
+
+                return undefined;
+            }
+        },
+        _clone: function (obj) {
+            return JSON.parse(JSON.stringify(obj));
+        }
+    };
+
+    return {JSONSharp: JSONSharp};
+}));
